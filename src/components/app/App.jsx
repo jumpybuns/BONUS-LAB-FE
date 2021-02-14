2d8c7327901b55e51a00764c33682507713bfa68
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-14 01:04:26 -0800

    goodnight

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReceiverPage from '../../containers/ReceiverPage';
+import Form from '../form/Form';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
+          <Form />
           <Switch>
             <Route
               path="/"
